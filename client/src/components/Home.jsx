74cec7ff8ca59cfa0a753ce0c96f905d4c9fd385
saksham218 <sakshamkumar03218@gmail.com>
2023-07-30 18:49:52 +0530

    added name, state and image

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
 import { useState } from "react";
+import { Typography, TextField, Menu, MenuItem, Select, FormControl, InputLabel, FormGroup, Box } from "@mui/material";
 import VideoRecorder from "./recorders/VideoRecorder";
 import AudioRecorder from "./recorders/AudioRecorder";
+import ImageCapturer from './recorders/ImageCapturer';
 
-const Home=()=>{
-    let [recordOption, setRecordOption] = useState("video");
-    const toggleRecordOption = (type) => {
-        return () => {
-            setRecordOption(type);
-        };
-    };
+const Home = () => {
+
+    const [name, setName] = useState("");
+    const [state, setState] = useState("");
+
+    const states = ['Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh',
+        'Jharkhand', 'Karnataka', 'Kearla', 'Madya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 'Rajasthan', 'Sikkim',
+        'Sikkim', 'Tamil Nadu', 'Telagana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal'];
     return (
         <div>
             <h1>Data Collection Platform</h1>
-            <div className="button-flex">
-                <button onClick={toggleRecordOption("video")}>
-                  Record Video
-                </button>
-                <button onClick={toggleRecordOption("audio")}>
-                  Record Audio
-                </button>
-            </div>
-            <div>
-                {recordOption === "video" ? <VideoRecorder /> : <AudioRecorder />}
-            </div>
+            <TextField label="Name" gutterBottom value={name} onChange={(e) => { setName(e.target.value) }} />
+
+            <Box >
+                <FormControl style={{ width: "225px" }}>
+                    <InputLabel>State</InputLabel>
+                    <Select label="State" value={state} onChange={(e) => setState(e.target.value)}>
+                        {states.map(s => <MenuItem value={s}>{s}</MenuItem>)}
+                    </Select>
+                </FormControl>
+            </Box>
+
+
+            <ImageCapturer />
+            <VideoRecorder />
+            <AudioRecorder />
         </div>
     );
 }
